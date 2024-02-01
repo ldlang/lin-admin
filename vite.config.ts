b350740ae013d518693eaf9bd9cfe4c44895942a
lindalang <1156614427@qq.com>
2024-02-01 21:33:54 +0800

    eslint 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint' // 引入刚刚下载的vite配合eslint的包
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
+    })
+  ]
 })
