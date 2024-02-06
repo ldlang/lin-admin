a5ba4e5483b64e47435623ac6e270d055f9fd30d
liuhongda <1156614427@qq.com>
2024-02-06 15:32:33 +0800

    normalize css库

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// windicss插件
 import 'virtual:windi.css'
+// 引入normalize样式库
+import 'normalize.css/normalize.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
