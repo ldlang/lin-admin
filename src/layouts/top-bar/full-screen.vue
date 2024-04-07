2871c6cdfe58e905b7d9557f8590d95d8d42d089
lindalang <1156614427@qq.com>
2024-04-07 14:42:47 +0800

    修复全屏组件tip不关闭问题

--- src/layouts/top-bar/full-screen.vue
+++ src/layouts/top-bar/full-screen.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { ElTooltip } from 'element-plus'
 import { useFullscreen } from '@vueuse/core'
 const { isFullscreen, enter, exit } = useFullscreen()
+
+const tooltipRef = ref<InstanceType<typeof ElTooltip>>()
+watch(isFullscreen, ()=> {
+  tooltipRef.value?.hide()
+})
 </script>
 
 <template>
   <div class="flex items-center !w-20 h-20 mx-10">
-    <el-tooltip :content="isFullscreen ? '退出全屏' : '全屏'" placement="bottom">
+    <el-tooltip ref="tooltipRef" :content="isFullscreen ? '退出全屏' : '全屏'" placement="bottom">
       <i v-if="!isFullscreen" @click="enter" class="iconfont icon-quanping hand !text-20" />
       <i v-if="isFullscreen" @click="exit" class="iconfont icon-tuichuquanping hand !text-17" />
     </el-tooltip>
