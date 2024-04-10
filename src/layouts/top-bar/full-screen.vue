552f8ddcacd08de55f1cf6456e38d419496dc7b1
lindalang <1156614427@qq.com>
2024-04-10 20:31:06 +0800

    修复全屏组件失效问题

--- src/layouts/top-bar/full-screen.vue
+++ src/layouts/top-bar/full-screen.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ElTooltip } from 'element-plus'
 import { useFullscreen } from '@vueuse/core'
-const { isFullscreen, enter, exit } = useFullscreen()
+const { isFullscreen, toggle } = useFullscreen()
 
 const tooltipRef = ref<InstanceType<typeof ElTooltip>>()
 watch(isFullscreen, ()=> {
@@ -10,10 +10,10 @@ watch(isFullscreen, ()=> {
 </script>
 
 <template>
-  <div class="flex items-center !w-20 h-20 mx-10">
+  <div class="flex items-center !w-20 h-20 mx-10" @click="toggle">
     <el-tooltip ref="tooltipRef" :content="isFullscreen ? '退出全屏' : '全屏'" placement="bottom">
-      <i v-if="!isFullscreen" @click="enter" class="iconfont icon-quanping hand !text-20" />
-      <i v-if="isFullscreen" @click="exit" class="iconfont icon-tuichuquanping hand !text-17" />
+      <i v-if="!isFullscreen" class="iconfont icon-quanping hand !text-20" />
+      <i v-if="isFullscreen" class="iconfont icon-tuichuquanping hand !text-17" />
     </el-tooltip>
   </div>
 </template>
