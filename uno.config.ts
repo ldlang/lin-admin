d6bd4c355f7148e11f56b06fafb1c13b615ec4a1
lindalang <1156614427@qq.com>
2024-04-15 21:36:11 +0800

    增加unocss的分组写法和指令插件

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
 import presetRemToPx from '@unocss/preset-rem-to-px'
+// 分组写法插件 <div class="hover:(bg-gray-400 font-medium) font-(light mono)"/>
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+// 指令插件 @apply text-center my-0 font-medium;
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   rules: [
@@ -27,5 +31,9 @@ export default defineConfig({
     presetRemToPx({
       baseFontSize: 4
     })
+  ],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives()
   ]
 })
